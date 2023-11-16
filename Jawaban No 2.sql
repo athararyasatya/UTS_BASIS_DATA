45bf8fe47a74a4efcc65fca508fbee22a0cf0262
20220801029_Athar Aryasatya <133422563+athararyasatya@users.noreply.github.com>
2023-11-16 21:16:30 +0700

    Update Jawaban No 2.sql

--- Jawaban No 2.sql
+++ Jawaban No 2.sql
@@ -1,15 +1,8 @@
-INSERT INTO mahasiswa (Id, Nama_Mahasiswa, Nim_Mahasiswa, fakultas)
-VALUES
+INSERT INTO `mahasiswa` VALUES (1, 'Athar', '20220801029', 'Ilmu_komputer', 'Teknik_informatika');
+INSERT INTO `mahasiswa` VALUES (2, 'Arhan', '20220801030', 'Ilmu_Komputer', 'Teknik_informatika');
 
-  (1, 'Athar ', '20220801029', 'Ilmu_komputer');
+INSERT INTO `dosen` VALUES (1, 'Bahrul', '20031', 'S.Kom,M.Kom');
+INSERT INTO `dosen` VALUES (2, 'Baddie', '20032', 'S.Kom,M.Kom');
 
-
-INSERT INTO dosen (Id, Nama_Dosen, Nim_Dosen, pendidikan)
-VALUES
-  (1, 'Bahrul', '20031', 'S.Kom,M.kom');
-
-INSERT INTO matakuliah (Id, Mata_Kuliah, Kode_Mata_Kuliah, pengajar)
-VALUES
-  (1, 'Basis_data', 111, ' Bahrul');
-
-
+INSERT INTO `matakuliah` VALUES (1, 'Basis_data', 1111, 'Bahrul');
+INSERT INTO `matakuliah` VALUES (2, 'PBO', 2222, 'Baddie');
